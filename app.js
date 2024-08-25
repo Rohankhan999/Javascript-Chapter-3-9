49fbbb1a0d5adc4b9e86d2dfad3233b624632573
Rohankhan999 <159823468+Rohankhan999@users.noreply.github.com>
2024-08-25 12:14:33 +0500

    cw

---
+++ app.js
@@ -0,0 +1,28 @@
+// Assigment no 02//
+
+// Question no 01//
+
+// var Age = "I am 17 years Old";
+// alert(Age);
+
+// Question no 02//
+
+// var n = 10;
+// var visit = "You have visited this site";
+// var time = "times";
+// alert(visit  +n  + 'time')
+
+
+// document.write("<P>My birth Year is 2007<P>")
+// var type = " Data type of my decleared variabile is ";
+// var year = 2007;
+// document.write(type + typeof year);
+
+
+// Question no 04//
+
+var visitor = "<b>jhon Doe</b>";
+var tshirts = " <b>5 t-shirts(s) </b>"
+document.write(visitor + " ordered" + tshirts+"on <b>XYZ Clothing Store</b>);
+
+ 
