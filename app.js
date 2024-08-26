5634884e156e54b722d5e86a3f8a9e07dcadc622
Rohankhan999 <rohankhan08453@gmail.com>
2024-08-26 16:40:44 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
-Assigment no 02//
+// Assigment no 02//
 
-Chapter no 03 //(VARIABLES FOR NUMBERS)
+// Chapter no 03 //(VARIABLES FOR NUMBERS)
 
-Question no 01//
+// Question no 01//
 
-var age = 17;
-alert("You are " + age  + " years old");
+// var age = 17;
+// alert("You are " + age  + " years old");
 
-Question no 02//
+// Question no 02//
 
-var n = 10;
-var visit = "You have visited this site";
-var time = "times";
-alert(visit  +  n  + time)
+// var n = 10;
+// var visit = "You have visited this site";
+// var time = "times";
+// alert(visit  +  n  + time)
 
-Question no 03//
+// Question no 03//
 
-document.write("<P>My birth Year is 2007<P>")
-var type = " Data type of my decleared variabile is ";
-var year = 2007;
-document.write(type + typeof year);
+// document.write("<P>My birth Year is 2007<P>")
+// var type = " Data type of my decleared variabile is ";
+// var year = 2007;
+// document.write(type + typeof year);
 
 
-Question no 04//
+// Question no 04//
 
-var visitor = "<b>jhon Doe</b>";
-var tshirts = " <b>5 t-shirts(s) </b>"
-document.write(visitor + " ordered" + tshirts+ "on <b>XYZ Clothing Store</b>");
+// var visitor = "<b>jhon Doe</b>";
+// var tshirts = " <b>5 t-shirts(s) </b>"
+// document.write(visitor + " ordered" + tshirts+ "on <b>XYZ Clothing Store</b>");
 
-Chapter no 04 //(LEGAL & ILLEGAL)
+// Chapter no 04 //(LEGAL & ILLEGAL)
 
-Question no 01//
+// Question no 01//
 
-var a , b , c = "Rohan";
+// var a , b , c = "Rohan";
 
-Question no 02//
+// Question no 02//
 
-Legal Names//
-1: var My_name ;
-2: var $father_age;
-3: var _my_age;
-4: var My_house_address;
-5: var myVariableExample;
+// Legal Names//
+// 1: var My_name ;
+// 2: var $father_age;
+// 3: var _my_age;
+// 4: var My_house_address;
+// 5: var myVariableExample;
 
-Question no 03//
+// Question no 03//
 
-document.write("<h1>“Rules for naming JS variables”</h1>" +
-    "<p>Variable names can only contain $ , Numbers  and _  For Example : $my_1stVariable</p>" +
-"<p>Variable must began with a letter, $ or _ . For Example $name , _name, or name.</p>" +
-"<p>Variable names are Case Sensitive</p> " + 
-"<p>Variable names should no be JS Keywords.</p>" )
+// document.write("<h1>“Rules for naming JS variables”</h1>" +
+//     "<p>Variable names can only contain $ , Numbers  and _  For Example : $my_1stVariable</p>" +
+// "<p>Variable must began with a letter, $ or _ . For Example $name , _name, or name.</p>" +
+// "<p>Variable names are Case Sensitive</p> " + 
+// "<p>Variable names should no be JS Keywords.</p>" )
 
 
 
